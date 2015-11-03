c0b840b20d5bcd7b703f2d3ab7a1f2b9a4cfdcf6
rojaware <leesungki@gmail.com>
2015-11-03 00:37:13 -0500

    init...

---
+++ src/main/java/net/sf/dynamicreports/examples/CurrentDirectoryExample.java
@@ -0,0 +1,11 @@
+package net.sf.dynamicreports.examples;
+
+public class CurrentDirectoryExample {
+	  
+    public static void main(String args[]) {
+      
+        String current = System.getProperty("user.dir");
+        System.out.println("Current working directory in Java : " + current);
+      
+    }
+}
